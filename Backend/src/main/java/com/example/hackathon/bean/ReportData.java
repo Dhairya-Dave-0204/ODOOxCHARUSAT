f7a72670643d6203ee4dcf3bc7436f5ed5598ca0
Dhruv01H <hirenmehtadhruv@gmail.com>
2025-03-02 10:03:14 +0530

    Pdf generator& model

---
+++ Backend/src/main/java/com/example/hackathon/bean/ReportData.java
@@ -0,0 +1,187 @@
+    // package com.example.hackathon.bean;
+
+    // public class ReportData {
+    //     private String patientName;
+    //     private String patientId;
+    //     private String examType;
+    //     private String indication;
+    //     private String comparison;
+    //     private String impression;
+    //     private String findings;
+    //     private String radiologistName;
+       
+    //     String Diaphragm; String HeartFailure; String Lung; 
+    //     String LungOpacity; String Opacity; String Pnuemothorax; 
+    //     String Pulmonary; String Congestion; String Disease; 
+    //     String Demphysema; String fibriosis; String Artery; 
+    //     String Granuloma;
+
+    //     public void setDiaphragm(String diaphragm) {
+    //         Diaphragm = diaphragm;
+    //     }
+
+    //     public void setHeartFailure(String heartFailure) {
+    //         HeartFailure = heartFailure;
+    //     }
+
+    //     public void setLung(String lung) {
+    //         Lung = lung;
+    //     }
+
+    //     public void setLungOpacity(String lungOpacity) {
+    //         LungOpacity = lungOpacity;
+    //     }
+
+    //     public void setOpacity(String opacity) {
+    //         Opacity = opacity;
+    //     }
+
+    //     public void setPnuemothorax(String pnuemothorax) {
+    //         Pnuemothorax = pnuemothorax;
+    //     }
+
+    //     public void setPulmonary(String pulmonary) {
+    //         Pulmonary = pulmonary;
+    //     }
+
+    //     public void setCongestion(String congestion) {
+    //         Congestion = congestion;
+    //     }
+
+    //     public void setDisease(String disease) {
+    //         Disease = disease;
+    //     }
+
+    //     public void setDemphysema(String demphysema) {
+    //         Demphysema = demphysema;
+    //     }
+
+    //     public void setFibriosis(String fibriosis) {
+    //         this.fibriosis = fibriosis;
+    //     }
+
+    //     public void setArtery(String artery) {
+    //         Artery = artery;
+    //     }
+
+    //     public void setGranuloma(String granuloma) {
+    //         Granuloma = granuloma;
+    //     }
+
+    //     public String getDiaphragm() {
+    //         return Diaphragm;
+    //     }
+
+    //     public String getHeartFailure() {
+    //         return HeartFailure;
+    //     }
+
+    //     public String getLung() {
+    //         return Lung;
+    //     }
+
+    //     public String getLungOpacity() {
+    //         return LungOpacity;
+    //     }
+
+    //     public String getOpacity() {
+    //         return Opacity;
+    //     }
+
+    //     public String getPnuemothorax() {
+    //         return Pnuemothorax;
+    //     }
+
+    //     public String getPulmonary() {
+    //         return Pulmonary;
+    //     }
+
+    //     public String getCongestion() {
+    //         return Congestion;
+    //     }
+
+    //     public String getDisease() {
+    //         return Disease;
+    //     }
+
+    //     public String getDemphysema() {
+    //         return Demphysema;
+    //     }
+
+    //     public String getFibriosis() {
+    //         return fibriosis;
+    //     }
+
+    //     public String getArtery() {
+    //         return Artery;
+    //     }
+
+    //     public String getGranuloma() {
+    //         return Granuloma;
+    //     }
+
+    //     // Getters and setters
+    //     public String getPatientName() {
+    //         return patientName;
+    //     }
+
+    //     public void setPatientName(String patientName) {
+    //         this.patientName = patientName;
+    //     }
+
+    //     public String getPatientId() {
+    //         return patientId;
+    //     }
+
+    //     public void setPatientId(String patientId) {
+    //         this.patientId = patientId;
+    //     }
+
+    //     public String getExamType() {
+    //         return examType;
+    //     }
+
+    //     public void setExamType(String examType) {
+    //         this.examType = examType;
+    //     }
+
+    //     public String getIndication() {
+    //         return indication;
+    //     }
+
+    //     public void setIndication(String indication) {
+    //         this.indication = indication;
+    //     }
+
+    //     public String getComparison() {
+    //         return comparison;
+    //     }
+
+    //     public void setComparison(String comparison) {
+    //         this.comparison = comparison;
+    //     }
+
+    //     public String getImpression() {
+    //         return impression;
+    //     }
+
+    //     public void setImpression(String impression) {
+    //         this.impression = impression;
+    //     }
+
+    //     public String getFindings() {
+    //         return findings;
+    //     }
+
+    //     public void setFindings(String findings) {
+    //         this.findings = findings;
+    //     }
+
+    //     public String getRadiologistName() {
+    //         return radiologistName;
+    //     }
+
+    //     public void setRadiologistName(String radiologistName) {
+    //         this.radiologistName = radiologistName;
+    //     }
+    // }
